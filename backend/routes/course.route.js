894d01047fa368ddd37784ad88382c1a0881f5ba
ayaabdellatif47-cmyk <ayaabdellatif47@gmail.com>
2026-09-03 16:34:26 +0300

    Update course routes to modify GET endpoints

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -7,11 +7,9 @@ const adminMiddleware = require("../middleware/admin.middleware")
 router.use(authMiddleware)
 
 router.post("/", adminMiddleware, courseController.createCourse)
-router.get("/", courseController.getCourses)
 router.get("/getall", courseController.getCourses)
 router.get("/search", courseController.getCoursesByTitle)
 router.get("/getbyid/:id", courseController.getCourseById)
-router.get("/:id", courseController.getCourseById)
 router.delete("/:id", adminMiddleware, courseController.deleteCourse)
 router.put("/:id", adminMiddleware, courseController.updateCourse)
 
